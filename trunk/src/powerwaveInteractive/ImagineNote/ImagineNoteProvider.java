7a4eef9109a20439a86cb767e1f82983b88edaba
firsttimelove <firsttimelove@ab8783ce-dd8b-11dd-a8eb-ddf07f3c1838>
2009-02-01 17:02:12 +0000

    기본기능 완료 -새 메모 추가 -기존 메모 내용/제목 수정 -기존 메모 삭제
    ImagineNoteTitleEditor.java추가
    -ImagineNoteInput에서 "Set Title"버튼을 누르면 뜨는 activity.(layout: titleeditor.xml)
    
    ImagineNoteList에서 넘겨주는 action(ACTION_EDIT,ACTION_INSERT)에 따라 ImagineNoteInput에서 처리하도록 코드 작성 완료
    
    복잡한 것은 없었는데 ContentProvider를 어떻게 만들어 쓰는지가 어려웠던 것 같다. 
    확실히 getContentResolver()는 AndroidManifest.xml에 정의된 provider를 넘겨 주는듯.
    query/insert/update/delete시 uri끝에 id가 붙어 있는 경우에는 다른 인자값을 설정 안해도 됨.(_ID가 유일하니까)
    insert/update시 ContentValues설정이 중요함.
    query시 PROJECTION설정에 따라 넘어오는 column이 결정된다. 그냥 column이름을 가진 String의 배열이 PROJECTION이 된다. SQLite일경우 그냥 SQLiteOpenHelper 구현에 넘겨주면 된다.
    
    설명이 필요하기보다는 짤때 누락된 부분이 많아서 시간이 오래 걸렸으나, 딱히 참조할 문서가 별로 없어서 그런것 같음. 그림 저장하는게 조금 걸리기는 한데.. 뭐 어떻게 되겟지... 
    
    UI를 구성하고,
    그 UI가 넘어오는 ACTION에 따라 다르게 반응하고,
    넘어오는 URI를 어떻게 handling하고 데이터를 저장하며,
    데이터를 처리하기 위한 ContentProvider를 어떻게 맞춰서 구성하는지,
    에 대한 것들은 일단 완료되었다고 봄.
    
    다음 주는 위키 정리를 하자!

--- trunk/src/powerwaveInteractive/ImagineNote/ImagineNoteProvider.java
+++ trunk/src/powerwaveInteractive/ImagineNote/ImagineNoteProvider.java
@@ -23,8 +23,9 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Log;
-
 import java.util.HashMap;
+
+
 
 /**
  * study 내용은 아래 링크 참조
@@ -37,10 +38,16 @@ public class ImagineNoteProvider extends ContentProvider{
 	
 	private static final String TAG = "ImagineNoteProvider";
 	
-	private static final String databaseName="ImagineNoteDatabase.db";
+	private static HashMap<String, String> NotesProjectionMap;
+	private static final String databaseName="Database.db";
 	private static final int databaseVersion=1;
 	private static final String tableName_note="Notes";
 	
+	private static final UriMatcher sUriMatcher;
+	private DatabaseHelper dbHelper;
+	
+	private static final int NOTES = 1;
+	private static final int NOTE = 2;
 	
 	/** 
 	 * @author firsttimelove@gmail.com
@@ -51,35 +58,98 @@ public class ImagineNoteProvider extends ContentProvider{
 	 * 재정의 해야 한다.
 	 */
 	private static class DatabaseHelper extends SQLiteOpenHelper{
+		
+		private static String queryString = "";
+		
 		DatabaseHelper(Context context) {
 			super(context, databaseName, null, databaseVersion);
 		}
 		
 		@Override
 		public void onCreate(SQLiteDatabase db){
-			//db.execSQL()함수를 호출한다.
-			/*
-			db.execSQL("create table " + tableName_note + "("
-					)
-					*/
 			
+			//db.execSQL()함수를 호출한다.
+			queryString = "CREATE TABLE " + tableName_note + "("
+				+ Note._ID + " INTEGER PRIMARY KEY,"
+				+ Note.TITLE + " TEXT,"
+				+ Note.NOTE + " TEXT,"
+				+ Note.CREATED_DATE + " INTEGER,"
+				+ Note.MODIFIED_DATE + " INTEGER"
+				+ ");";
+			Log.println(2,ImagineNoteProvider.TAG ,"building database... query=" + queryString);
+			db.execSQL(queryString);
 		}
 
 		@Override
-		public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-			// 기존의 테이블을 삭제하고 onCreate()를 호출하면 된다.			
+		public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+			// 기존의 테이블을 삭제하고 onCreate()를 호출하면 된다.
+            db.execSQL("DROP TABLE IF EXISTS " + tableName_note);
+            onCreate(db);
 		}
 	}
 	
 	@Override
-    public boolean onCreate() {
-       
+    public boolean onCreate() {		
+		this.dbHelper = new DatabaseHelper(getContext());
+		Log.println(2, ImagineNoteProvider.TAG, "ImagineNoteProvider.onCreate() called.");
         return true;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-    	return null;        
+    public Cursor query(Uri uri,
+    		String[] projection,
+    		String selection,
+    		String[] selectionArgs, 
+    		String sortOrder) 
+    {
+    	SQLiteQueryBuilder qb = new SQLiteQueryBuilder();    	
+
+        switch (sUriMatcher.match(uri)) {
+	        case NOTES :
+	        	qb.setTables(ImagineNoteProvider.tableName_note);
+	        	
+	        	/**
+	        	 * public void setProjectionMap(Map<String, String> columnMap)
+	        	 * 
+	        	 * query에 대한 projection map을 설정한다. caller가 넘겨준 column name들을 database column name으로 
+	        	 * 매핑하는 역할을 한다. 
+	        	 *  
+	        	 */
+	            qb.setProjectionMap(ImagineNoteProvider.NotesProjectionMap);
+	            
+	        	break;
+	        case NOTE :
+	        	qb.setTables(ImagineNoteProvider.tableName_note);
+	            qb.setProjectionMap(ImagineNoteProvider.NotesProjectionMap);
+	        	qb.appendWhere(Note._ID + "=" + uri.getPathSegments().get(1));
+	        	break;
+        	default : 
+        		throw new IllegalArgumentException("Unknown URI " + uri);       		
+        		
+        }
+        Log.println(2, this.TAG, "query string = " + uri.toString() );
+	    	
+        // order by 구문 설정. 넘어온 값이 있으면 그 값을 설정하고, 없으면 기본값 설정.
+        String orderBy;
+        if (TextUtils.isEmpty(sortOrder)) {
+            orderBy = Note.DEFAULT_SORT_ORDER;
+        } else {
+            orderBy = sortOrder;
+        }
+        
+        // Get the database and run the query : 
+        SQLiteDatabase db = dbHelper.getReadableDatabase();        
+        Cursor c = qb.query(db, 
+        		projection, 
+        		selection, 
+        		selectionArgs, 
+        		null, 
+        		null, 
+        		orderBy);
+
+        // Tell the cursor what uri to watch, so it knows when its source data changes
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;        
     }
 
     @Override
@@ -87,18 +157,109 @@ public class ImagineNoteProvider extends ContentProvider{
         return "";
     }
 
-    @Override
     public Uri insert(Uri uri, ContentValues initialValues) {
-		return uri;        
+        // Validate the requested uri
+        if (sUriMatcher.match(uri) != NOTES) {
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        ContentValues values;
+        if (initialValues != null) {
+            values = new ContentValues(initialValues);
+        } else {
+            values = new ContentValues();
+        }
+
+        Long now = Long.valueOf(System.currentTimeMillis());
+
+        // Make sure that the fields are all set
+        if (values.containsKey(Note.CREATED_DATE) == false) {
+            values.put(Note.CREATED_DATE, now);
+        }
+
+        if (values.containsKey(Note.MODIFIED_DATE) == false) {
+            values.put(Note.MODIFIED_DATE, now);
+        }
+
+        if (values.containsKey(Note.TITLE) == false) {
+            Resources r = Resources.getSystem();
+            values.put(Note.TITLE, Note.DEFAULT_TITLE);
+        }
+
+        if (values.containsKey(Note.NOTE) == false) {
+            values.put(Note.NOTE, "");
+        }
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        long rowId = db.insert(tableName_note, Note.NOTE, values);
+        if (rowId > 0) {
+            Uri noteUri = ContentUris.withAppendedId(Note.CONTENT_URI, rowId);
+            getContext().getContentResolver().notifyChange(noteUri, null);
+            return noteUri;
+        }
+        throw new SQLException("Failed to insert row into " + uri);
     }
+
 
     @Override
     public int delete(Uri uri, String where, String[] whereArgs) {
-        return 0;
+    	int count = 0;
+    	String noteId;
+    	SQLiteDatabase db = dbHelper.getWritableDatabase();
+    	switch (sUriMatcher.match(uri)) {
+	        case NOTES:
+	            count = db.delete(ImagineNoteProvider.tableName_note,	            		
+	            		where, 
+	            		whereArgs);
+	            break;
+	
+	        case NOTE:
+	            noteId = uri.getPathSegments().get(1);
+	            count = db.delete(ImagineNoteProvider.tableName_note,
+	            		Note._ID + "=" + noteId + (!TextUtils.isEmpty(where)?" AND(" + where + ")" : ""), 
+	            		whereArgs);
+	            break;
+	
+	        default:
+	            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        return count;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
-        return 0;
+    	SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int count;
+        String noteId;
+        switch (sUriMatcher.match(uri)) {
+        case NOTES:
+            count = db.update(ImagineNoteProvider.tableName_note, values, where, whereArgs);
+            break;
+
+        case NOTE:
+            noteId = uri.getPathSegments().get(1);
+            count = db.update(ImagineNoteProvider.tableName_note, values, Note._ID + "=" + noteId
+                    + (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+    
+    static {
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        sUriMatcher.addURI(Note.AUTHORITY, "Notes", NOTES);
+        sUriMatcher.addURI(Note.AUTHORITY, "Notes/#", NOTE );        
+        
+        NotesProjectionMap = new HashMap<String, String>();
+        NotesProjectionMap.put(Note._ID, Note._ID);
+        NotesProjectionMap.put(Note.TITLE, Note.TITLE);
+        NotesProjectionMap.put(Note.NOTE, Note.NOTE);
+        NotesProjectionMap.put(Note.CREATED_DATE, Note.CREATED_DATE);
+        NotesProjectionMap.put(Note.MODIFIED_DATE, Note.MODIFIED_DATE);
     }
 }
